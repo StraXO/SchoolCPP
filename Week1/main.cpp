b0cdc2b0d63dd1860133e42eb8c47602e06c6238
Thomas Holtkamp <uitgeopt1@gmail.com>
2022-04-29 19:37:06 +0200

    Finish initial program, not splitted

--- Week1/main.cpp
+++ Week1/main.cpp
@@ -6,29 +6,15 @@ using namespace std;
 
 int randomNumber();
 int readNumber();
+bool endGame();
+void startGame();
 
 int main() {
-    int guessCount{1};
-    std::cout << "Let's play a game. I'm thinking of a number." << std::endl;
-    std::cout << "You have 7 tries to guess what it is." << std::endl;
+    bool shouldStop = false;
 
-    int numberToGuess = randomNumber();
-
-    while (guessCount <= 7) {
-        int guessInput = readNumber();
-        cout << "Guess #" << guessCount << ": " << guessInput << endl;
-
-        if (numberToGuess < guessInput) {
-            cout << "Your guess is too high." << endl;
-        } else if (numberToGuess > guessInput) {
-            cout << "Your guess is too low." << endl;
-        } else if (numberToGuess == guessInput) {
-            cout << "Correct! You win!" << endl;
-        } else {
-            cout << "Sorry, you lose. The correct number was " << numberToGuess << "." << endl;
-        }
-
-        guessCount++;
+    while (!shouldStop) {
+        startGame();
+        shouldStop = endGame();
     }
 
     return 0;
@@ -37,20 +23,73 @@ int main() {
 int randomNumber()
 {
     // Initialize our mersenne twister with a random seed based on the clock
-    std::mt19937 mersenne(static_cast<unsigned int>(std::time(nullptr)));
-    // Create a reusable random number generator
-    std::uniform_int_distribution<> generator(1, 100);
+    mt19937 mersenne(static_cast<unsigned int>(time(nullptr)));
 
+    // Create a reusable random number generator
+    uniform_int_distribution<> generator(1, 100);
     return generator(mersenne);
 }
 
 int readNumber() {
     string guessInput;
+    int inputInt;
+
     try {
         cin >> guessInput;
-        return stoi(guessInput);
-    } catch (std::exception& msg) {
+        inputInt = stoi(guessInput);
+        while (inputInt < 1 || inputInt > 100) {
+            cout << "Input must be between 1 and 100" << endl;
+            cin >> guessInput;
+            inputInt = stoi(guessInput);
+        }
+    } catch (exception& msg) {
+        return readNumber();
     }
 
-    return readNumber();
+    return inputInt;
+}
+
+void startGame() {
+    int currentGuessCount{1};
+    const int numberToGuess = randomNumber();
+
+    cout << "Let's play a game. I'm thinking of a number." << endl;
+    cout << "You have 7 tries to guess what it is." << endl;
+
+    // start game
+    while (currentGuessCount <= 7) {
+        cout << "Guess #" << currentGuessCount << ": ";
+        const int guessInput = readNumber();
+
+        if (numberToGuess < guessInput) {
+            cout << "Your guess is too high." << endl;
+        } else if (numberToGuess > guessInput) {
+            cout << "Your guess is too low." << endl;
+        } else {
+            cout << "Correct! You win!" << endl;
+            return;
+        }
+
+        currentGuessCount++;
+    }
+
+    cout << "Sorry, you lose. The correct number was " << numberToGuess << "." << endl;
+}
+
+bool endGame() {
+    // ask for replay
+    string input;
+    while (input != "y" || input != "n") {
+        cout << "Would you like to play again (y/n)? ";
+        cin >> input;
+
+        if (input == "y") {
+            startGame();
+        } else if (input == "n") {
+            cout << "Thank you for playing." << endl;
+            return true;
+        }
+    }
+
+    return false;
 }
